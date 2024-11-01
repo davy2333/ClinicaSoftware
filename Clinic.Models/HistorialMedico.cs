032b79f00d0e0c3cc78a87a4b29852ecc8407541
davy <144752667+davy2333@users.noreply.github.com>
2024-11-01 11:41:50 -0600

    configuracion de entity frmawork por segunda vez luego de que se me arruinara

---
+++ Clinic.Models/HistorialMedico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clinic.DAL.DataContext;
+
+public partial class HistorialMedico
+{
+    public int Id { get; set; }
+
+    public int? PacienteId { get; set; }
+
+    public DateTime? Fecha { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public int? TratamientoId { get; set; }
+
+    public int? MedicoId { get; set; }
+
+    public virtual Medico? Medico { get; set; }
+
+    public virtual Paciente? Paciente { get; set; }
+
+    public virtual Tratamiento? Tratamiento { get; set; }
+}
